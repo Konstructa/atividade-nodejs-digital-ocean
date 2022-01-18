9dd1a21e340aaa8fb4b2796b55b5208579985086
Konstructa <elenprojects7@gmail.com>
2022-01-18 13:17:16 -0300

    adicionando mongdb e querys

--- index.js
+++ index.js
@@ -1,107 +1,148 @@
 const express = require('express');
-const app = express();
+const {MongoClient, ObjectId} = require('mongodb')
+
+const url = "mongodb://localhost:27017";
+const dbName = "aula_ocean";
 const port = 3000;
 
 const listaHerois = ["Capitã América", "Viuva Negra", "Batwoman"]
 
-//body das req serão em json
-app.use(express.json());
+async function main () {
+
+    //conexao com o banco de dados
+
+    const client = await MongoClient.connect(url);
+
+    const db = client.db(dbName);
+
+    const collection = db.collection('herois')
 
 
-app.get('/', (res) => {
-    res.send("Hello World");
-})
+    //Aplicação
+    //body das req serão em json
+    const app = express();
 
 
-app.get('/heroi', (req, res) => {
-
-    //Boolean so retorna true, valores que existem
-    res.status(200).json(
-        {
-            valor: listaHerois.filter(Boolean)
-        }
-        );
-})
+    app.use(express.json());
 
 
-app.get('/heroi/:id', (req, res) => {
-    //para que não exista heroi 0
-    const id = req.params.id - 1;
-    if (!listaHerois[id]){
-        res.status(404).send(
-           "Heroi não encontrado"
-           );
-        
-        return
-    }
-    res.status(302).json(
-        {
-            valor: listaHerois[id]
-        }
-        );
-})
+    app.get('/', (req, res) => {
+        res.send("Hello World");
+    })
 
 
-app.post('/heroi', (req, res) => {
+    app.get('/heroi', async (req, res) => {
 
-    const registro = req.body.nome;
+        const herois = await collection.find().toArray();
 
-    if (!registro || registro === ""){
-        res.status(406).send(
-            "Você não enviou um dado"
-            ); 
-
-        return       
-    }
-
-    listaHerois.unshift(registro);
-
-    res.status(201).send(
-        "Criado com sucesso!"
-        );
-
-});
+        //Boolean so retorna true, valores que existem
+        res.status(200).json(
+            {
+                valor: herois
+            }
+            );
+    })
 
 
-app.put('/heroi/:id', (req, res) => {
-    const id = req.params.id - 1;
+    app.get('/heroi/:id', async (req, res) => {
+        //para que não exista heroi 0
+        const id = req.params.id;
 
-    if (!listaHerois[id] ||
-        !req.body.nome ||
-        req.body.nome === ""
-        ){
-        res.status(406).send(
-            "Você digitou o ID errado e/ou não inseriu o valor de mudança"
-        )
-    }
+        const item = await collection.findOne({ _id: new ObjectId(id)})
 
-    listaHerois[id] = req.body.nome;
-
-    res.status(320).send("Ok")
-});
-
-
-app.delete('/heroi/:id', (req, res) => {
-    //ou req.body e dps so o id.id
-    const id = req.params.id - 1;
-
-    if (!listaHerois[id]) {
-        res.status(404).send(
+        if (!item){
+            res.status(404).send(
             "Heroi não encontrado"
             );
-        
-        return
-    }
+            
+            return
+        } 
 
-    delete listaHerois[id]
+        res.status(302).json(
+            {
+                valor: item
+            }
+            );
+    })
 
-    res.status(202).send(
-        "Heroi deletado"
+
+    app.post('/heroi', async (req, res) => {
+
+        const registro = req.body;
+
+        if (!registro || 
+            registro === ""
+            ){
+            res.status(406).send(
+                "Você não enviou um dado"
+                ); 
+
+            return       
+        }
+
+        await collection.insertOne(registro)
+
+        res.status(201).send(
+                registro
+            );
+
+    });
+
+
+    app.put('/heroi/:id', async (req, res) => {
+        const id = req.params.id;
+
+        const item = req.body;
+
+        const isFound = await collection.countDocuments({ _id: new ObjectId(id) });
+
+        if (!isFound ||
+            !req.body.nome ||
+            req.body.nome === ""
+            ){
+            res.status(406).send(
+                "Você digitou o ID errado e/ou não inseriu o valor de mudança"
+            )
+        } 
+
+        collection.updateOne(
+            {_id: new ObjectId(id)},
+            {
+                $set: item, 
+            }
         );
 
-});
+        res.status(320).send(item)
+    });
 
 
-app.listen(port, () => {
-    console.log(`Servidor iniciado na porta ${port}`)
-});
+    app.delete('/heroi/:id', async(req, res) => {
+        //ou req.body e dps so o id.id
+        const id = req.params.id;
+
+        /* const isFound = await collection.findOne({ _id: new ObjectId(id) });
+
+         if (!isFound) {
+            res.status(404).send(
+                "Heroi não encontrado"
+                );
+            
+            return
+        }  */
+
+        await collection.deleteOne({_id : new ObjectId(id)})
+
+        res.status(202).send(
+            "Heroi deletado"
+            );
+
+    });
+
+
+    app.listen(port, () => {
+        console.log(`Servidor iniciado na porta ${port}`)
+    });
+
+    }
+
+main()
